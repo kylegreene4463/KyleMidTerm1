283f5c921dbe91ec90d85907388f10eb3c77b17a
kylegreene4463 <kyle.greene741@gmail.com>
2023-02-23 16:00:25 -0500

    canged names of module, and project

---
+++ app/src/main/java/kyle/greene/n01534463/mid/KyleGre1/KylActivity.java
@@ -0,0 +1,14 @@
+package kyle.greene.n01534463.mid.KyleGre1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class KylActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
